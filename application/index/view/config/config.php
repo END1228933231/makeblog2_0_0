9005a1f679427cb50ea2e846f1a4778bf6b69ecf
END1228933231 <32391920+END1228933231@users.noreply.github.com>
2018-07-16 12:52:59 +0800

    make
    andjklsjljd

---
+++ application/index/view/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'view_replace_str' => [
+        '__IMG__' => 'static/images',
+        '__CSS__' => 'static/css',
+        '__JS__' => 'static/js',
+        '__STATIC__' => 'static'
+    ]
+];
